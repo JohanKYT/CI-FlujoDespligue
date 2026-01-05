74e1c15383f484c31f4313c28cbe3cd7a2cf8cae
JohanKYT <kevin.maquis@udla.edu.ec>
2026-01-05 17:17:29 -0500

    Commit inicial

---
+++ src/main/java/org/example/ciflujodespligue/CiFlujoDespligueApplication.java
@@ -0,0 +1,13 @@
+package org.example.ciflujodespligue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiFlujoDespligueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiFlujoDespligueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ciflujodespligue/CiFlujoDespligueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ciflujodespligue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiFlujoDespligueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
